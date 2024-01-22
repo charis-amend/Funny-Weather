a5768d4185995222b36f09608c181c1cc027dd4b
Charis Amend <charis.amend@googlemail.com>
2024-01-22 18:45:03 +0100

    added activites, fetch api, temp and condition emoji header

--- weather-app/src/components/List.jsx
+++ weather-app/src/components/List.jsx
@@ -1,15 +1,18 @@
 /* eslint-disable react/prop-types */
 // eslint-disable-next-line react/prop-types
-export default function List({ activities }) {
+export default function List({ activities, instructions }) {
 
     return (
-        <ul className="activity-list">
-            {activities.map((activity) => (
-                <li key={activity.id} className="list-item">
-                    {activity.name}
-                </li>
-            ))
-            }
-        </ul>
+        <>
+            <h3 className="instructions-list">{instructions}</h3>
+            <ul className="activity-list">
+                {activities.map((activity) => (
+                    <li key={activity.id} className="list-item">
+                        {activity.name}
+                    </li>
+                ))
+                }
+            </ul>
+        </>
     )
 }
