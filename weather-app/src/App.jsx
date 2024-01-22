b45b59f6b4bb679388be9bb78967bccb10a9d86c
Charis Amend <charis.amend@googlemail.com>
2024-01-22 20:34:34 +0100

    added weather design components

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -3,17 +3,14 @@ import { useEffect, useState } from 'react'
 import './App.css'
 import Form from './components/Form.jsx'
 import List from './components/List.jsx'
+import WeatherBackground from './components/WeatherBackground.jsx'
 // import { useState } from 'react'
 import { uid } from 'uid'
 import useLocalStorageState from 'use-local-storage-state'
 
-
-
-
-
 function App() {
   const [activities, setActivities] = useLocalStorageState("activities", { defaultValue: [] })
-  const [weather, setWeather] = useState();
+  const [weather, setWeather] = useState("");
   useEffect(() => {
     async function startFetching() {
 
@@ -29,12 +26,14 @@ function App() {
     setActivities([...activities, { ...newActivity, id: uid() }])
   }
   const isGoodWeather = weather?.isGoodWeather
-  console.log(isGoodWeather)
+  console.log("the condition whether isGoodWeather from the api", isGoodWeather)
+  console.log("activities:", activities)
   const goodWeatherActivities = activities.filter(activity =>
-    activity.isGoodWeather === true
+    activity.isForGoodWeather === true
   )
+  console.log(goodWeatherActivities)
   const badWeatherActivities = activities.filter(activity =>
-    activity.isGoodWeather === false
+    activity.isForGoodWeather === false
   )
 
   return (
@@ -43,14 +42,16 @@ function App() {
         <h1>Weather App</h1>
       </div>
       <section className='weather api'>
-        <h4 id='emoji-weather'>{weather?.condition}</h4>
+        {/* <h4 id='emoji-weather'>{weather?.condition}</h4> */}
+        <WeatherBackground className='weather-background' currentWeatherCondition={weather?.condition} />
         <h4 id='temperature-weather'>{weather?.temperature} °C</h4>
       </section>
       <List
         instructions={isGoodWeather ? "The weather is awesome! Go outside and:" : "Bad weather outside! Here's what you can do now:"}
         activities={isGoodWeather ? goodWeatherActivities : badWeatherActivities}>
-      </List>
+      </List >
       <Form onAddActivity={AddActivity} />
+      {/* <WeatherBackground className='weather-background' currentWeatherCondition={weather?.condition} /> */}
     </>
   )
 }
---
+++ weather-app/src/components/WeatherBackground.jsx
@@ -0,0 +1,100 @@
+
+// eslint-disable-next-line react/prop-types
+export default function WeatherBackground({ currentWeatherCondition }) {
+
+    let WeatherComponent;
+
+    switch (currentWeatherCondition) {
+        case '🌤️':
+            WeatherComponent = SunAndShower;
+            break;
+        case '⛈️':
+            WeatherComponent = Thunderstorm;
+            break;
+        case '☁️':
+            WeatherComponent = Cloudy;
+            break;
+        case '❄️':
+            WeatherComponent = Snow;
+            break;
+        case "☀️":
+            WeatherComponent = Sunny;
+            break;
+        case '🌧️':
+            WeatherComponent = Rainy;
+            break;
+        default:
+            WeatherComponent = null;
+    }
+
+    return (
+        <>
+            {WeatherComponent && <WeatherComponent />}
+        </>
+    );
+
+}
+
+function SunAndShower() {
+    return (
+        <div className="icon sun-shower">
+            <div className="cloud"></div>
+            <div className="sun">
+                <div className="rays"></div>
+            </div>
+        </div>
+    )
+}
+
+function Thunderstorm() {
+    return (
+        <div className="icon thunder-storm">
+            <div className="cloud"></div>
+            <div className="lightning">
+                <div className="bolt"></div>
+                <div className="bolt"></div>
+            </div>
+        </div>
+    )
+}
+
+function Cloudy() {
+    return (
+        <div className="icon cloudy">
+            <div className="cloud"></div>
+            <div className="cloud"></div>
+        </div>
+    )
+}
+
+function Snow() {
+    return (
+
+        <div className="icon flurries">
+            <div className="cloud"></div>
+            <div className="snow">
+                <div className="flake"></div>
+                <div className="flake"></div>
+            </div>
+        </div>
+    )
+}
+
+function Sunny() {
+    return (
+        <div className="icon sunny">
+            <div className="sun">
+                <div className="rays"></div>
+            </div>
+        </div>
+    )
+}
+
+function Rainy() {
+    return (
+        <div className="icon rainy">
+            <div className="cloud"></div>
+            <div className="rain"></div>
+        </div>
+    )
+}
