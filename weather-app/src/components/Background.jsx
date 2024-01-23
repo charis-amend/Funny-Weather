81e007c41ba62ef873c0664e5dcf1c0fb500c116
Charis Amend <charis.amend@googlemail.com>
2024-01-23 17:55:07 +0100

    updatedd input name field, checkbox

--- weather-app/src/components/Background.jsx
+++ weather-app/src/components/Background.jsx
@@ -1,115 +1,118 @@
-import sunandshowerBackground from "../assets/sunandshower-background-img.jpg"
-import thunderstormBackground from "../assets/thunderstorm-background-img.jpg"
-import cloudyBackground from "../assets/cloudy-background-img.jpg"
-import snowBackground from "../assets/snow-background-img.jpg"
-import sunnyBackground from "../assets/sunny-background-img.jpg"
-import rainyBackground from "../assets/rainy-background-img.jpg"
+// import sunnyandshowerBackground from "../assets/sunnyandshower-background-img.jpg"
+// import thunderstormBackground from "../assets/thunderstorm-background-img.jpg"
+// import cloudyBackground from "../assets/cloudy-background-img.jpg"
+// import snowBackground from "../assets/snow-background-img.jpg"
+// import sunnyBackground from "../assets/sunny-background-img.jpg"
+// import rainyBackground from "../assets/rainy-background-img.jpg"
 
-// eslint-disable-next-line react/prop-types
-export default function Background({ currentWeatherCondition }) {
+// // eslint-disable-next-line react/prop-types
+// export default function SwitchBackground({ currentWeatherImg, children }) {
 
-    let WeatherImg;
+//     let WeatherImg;
 
-    switch (currentWeatherCondition) {
-        case '🌤️':
-            WeatherImg = SunAndShowerImg;
-            break;
-        case '⛈️':
-            WeatherImg = ThunderstormImg;
-            break;
-        case '☁️':
-            WeatherImg = CloudyImg;
-            break;
-        case '❄️':
-            WeatherImg = SnowImg;
-            break;
-        case "☀️":
-            WeatherImg = SunnyImg;
-            break;
-        case '🌧️':
-            WeatherImg = RainyImg;
-            break;
-        default:
-            WeatherImg = null;
-    }
+//     switch (currentWeatherImg) {
+//         case '🌤️':
+//             WeatherImg = SunAndShowerImg;
+//             break;
+//         case '⛈️':
+//             WeatherImg = ThunderstormImg;
+//             break;
+//         case '☁️':
+//             WeatherImg = CloudyImg;
+//             break;
+//         case '❄️':
+//             WeatherImg = SnowImg;
+//             break;
+//         case "☀️":
+//             WeatherImg = SunnyImg;
+//             break;
+//         case '🌧️':
+//             WeatherImg = RainyImg;
+//             break;
+//         default:
+//             WeatherImg = null;
+//     }
 
-    return (
-        <>
-            {WeatherImg && <WeatherImg />}
-        </>
-    );
+//     return (
+//         <>
+//             {WeatherImg && <WeatherImg />}
+//             {children}
 
-}
+//         </>
+//     );
 
-function SunAndShowerImg() {
-    return (
-        <>
-            <div className="background-app">
-                <img className="sunandshower-background-img"
-                    src={sunandshowerBackground}
-                    alt="sunandshower-background" />
-            </div>
-        </>
-    )
-}
+// }
 
-function ThunderstormImg() {
-    return (
-        <>
-            <div className="background-app">
-                <img className="thunderstorm-background-img"
-                    src={thunderstormBackground}
-                    alt="thunderstorm-background" />
-            </div>
-        </>
-    )
-}
+// // eslint-disable-next-line react/prop-types
+// function SunAndShowerImg() {
+//     return (
+//         <>
+//             <div className="background-app">
+//                 <img className="sunnyandshower-background-img"
+//                     src={sunnyandshowerBackground}
+//                     alt="sunnyandshower-background" />
+//             </div>
+//         </>
+//     )
+// }
 
-function CloudyImg() {
-    return (
-        <>
-            <div className="background-app">
-                <img className="cloudy-background-img"
-                    src={cloudyBackground}
-                    alt="cloudy-background" />
-            </div>
-        </>
-    )
-}
+// function ThunderstormImg() {
+//     return (
+//         <>
+//             <div className="background-app">
+//                 <img className="thunderstorm-background-img"
+//                     src={thunderstormBackground}
+//                     alt="thunderstorm-background" />
+//             </div>
+//         </>
+//     )
+// }
 
-function SnowImg() {
-    return (
+// function CloudyImg() {
+//     return (
+//         <>
+//             <div className="background-app">
+//                 <img className="cloudy-background-img"
+//                     src={cloudyBackground}
+//                     alt="cloudy-background" />
+//             </div>
+//         </>
+//     )
+// }
 
-        <>
-            <div className="background-app">
-                <img className="snow-background-img"
-                    src={snowBackground}
-                    alt="snow-background" />
-            </div>
-        </>
-    )
-}
+// function SnowImg() {
+//     return (
 
-function SunnyImg() {
-    return (
-        <>
-            <div className="background-app">
-                <img className="sunny-background-img"
-                    src={sunnyBackground}
-                    alt="sunny-background" />
-            </div>
-        </>
-    )
-}
+//         <>
+//             <div className="background-app">
+//                 <img className="snow-background-img"
+//                     src={snowBackground}
+//                     alt="snow-background" />
+//             </div>
+//         </>
+//     )
+// }
 
-function RainyImg() {
-    return (
-        <>
-            <div className="background-app">
-                <img className="rainy-background-img"
-                    src={rainyBackground}
-                    alt="rainy-background" />
-            </div>
-        </>
-    )
-}
+// function SunnyImg() {
+//     return (
+//         <>
+//             <div className="background-app">
+//                 <img className="sunny-background-img"
+//                     src={sunnyBackground}
+//                     alt="sunny-background" />
+//             </div>
+//         </>
+//     )
+// }
+
+// function RainyImg() {
+//     return (
+//         <>
+//             <div className="background-app">
+//                 <img className="rainy-background-img"
+//                     src={rainyBackground}
+//                     alt="rainy-background" />
+//             </div>
+//         </>
+//     )
+// }
